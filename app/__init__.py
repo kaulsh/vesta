72f09d32094ab78545a741234c60551337a3bd1e
Shashank Kaul <kaulshashank96@gmail.com>
2026-02-02 14:11:10 +0530

    Vibe coded a http upgrade

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, redirect, request
 
 from app.db import init_db
 from app.routes import main_bp
 
 
-def create_app() -> Flask:
+def create_app(redirect_to_https: bool = False) -> Flask:
     app = Flask(__name__, instance_relative_config=True)
 
     instance_path = Path(app.instance_path)
@@ -16,6 +16,14 @@ def create_app() -> Flask:
         DATABASE=str(instance_path / "vesta.db"),
     )
 
+    # If this is the HTTP redirect instance, redirect all traffic to HTTPS
+    if redirect_to_https:
+        @app.before_request
+        def redirect_to_https_handler():
+            # Get the host from the request
+            host = request.host.split(':')[0]  # Remove port if present
+            return redirect(f'https://{host}{request.path}', code=301)
+    
     init_db(app)
     app.register_blueprint(main_bp)
 
