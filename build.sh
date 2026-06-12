8c700c79bf33b8261bcde1656da75381a5f7134b
Shashank Kaul <kaulshashank96@gmail.com>
2026-06-12 11:33:05 +0530

    Removed old build scripts and docs

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Build the Vesta Docker image. Use this image in your own docker-compose elsewhere.
-set -e
-cd "$(dirname "$0")"
-docker build -t vesta:latest -f app/Dockerfile .
